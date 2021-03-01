c99254e9dc3e1a47e9b0e60334062302bd738722
Dimple Verma <40714858+dimple1702@users.noreply.github.com>
2021-03-01 12:02:49 +0530

    Project Created
    A new project created.

---
+++ CSharpPrimer/CSharpPrimer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPrimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
